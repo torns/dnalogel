657b71ae2691daf4a040c67a6be537e65e96e876
Angry-Sparrow <beqan2016@163.com>
2022-04-20 17:11:42 +0800

    refactor

--- plugins/bin/svelte-compile.js
+++ plugins/bin/svelte-compile.js
@@ -21,13 +21,12 @@ replace({
 const sveltes = glob.sync('**/*.svelte', { cwd: libsPath })
 
 const run = async () => {
-	sveltes.map((svelte) => path.resolve(libsPath, svelte)).forEach(async (svelte) => {
-		const code = ( await readFile(svelte) ).toString()
+	for (const svelte1 of sveltes.map((svelte) => path.resolve(libsPath, svelte))) {
+		const code = ( await readFile(svelte1) ).toString()
 		const { js } = compile(code, { css: true })
-		await writeFile(svelte.replace('.svelte', '.js'), js.code)
-		
-		await unlink(path.resolve(libsPath, svelte))
-	})
+		await writeFile(svelte1.replace('.svelte', '.js'), js.code)
+		await unlink(path.resolve(libsPath, svelte1))
+	}
 }
 
-run()
+run().then(r => console.log("✨ svelte compile done！ ✨"))
